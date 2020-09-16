52d7f96e17f0a9905f97051d0406954e174146b6
aerol457 <58309142+aerol457@users.noreply.github.com>
2020-09-16 11:47:22 +0300

    Created a hard coded burger

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import classes from "./Burger.css";
+import BurgerIngedient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = (props) => {
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngedient type="bread-top" />
+      <BurgerIngedient type="cheese" />
+      <BurgerIngedient type="meat" />
+      <BurgerIngedient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
